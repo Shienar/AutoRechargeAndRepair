de2778bcde4827a700a2ae32d44da7825f49bc37
Shienar <grantviz@outlook.com>
2025-06-21 04:49:06 -0400

    added support for crown items.

--- AutoRechargeAndRepair/AutoRechargeAndRepair.lua
+++ AutoRechargeAndRepair/AutoRechargeAndRepair.lua
@@ -1,12 +1,14 @@
-AR = { name = "AutoRecharger" }
+AR = { name = "AutoRechargeAndRepair" }
 
 AR.defaults = {
 	debugMessages = true,
 	autoRecharge = true,
 	rechargePercentage = 10,
+	preferCrownGem = true,
 	autoRepair = true,
 	autoMerchant = true,
 	repairPercentage = 25,
+	useCrownRepair = false,
 }
 
 local function attemptCharge(weapon, gem)
@@ -14,6 +16,7 @@ local function attemptCharge(weapon, gem)
 		local charges, maxCharges = GetChargeInfoForItem(BAG_WORN, weapon)
 		if (charges/maxCharges) < (AR.savedVariables.rechargePercentage/100) then
 			ChargeItemWithSoulGem(BAG_WORN, weapon, BAG_BACKPACK, gem)
+			PlaySound(SOUNDS.INVENTORY_ITEM_APPLY_CHARGE)
 			if AR.savedVariables.debugMessages then d("Item charged: "..GetItemName(BAG_WORN, weapon)) end
 		end
 	end
@@ -24,6 +27,7 @@ local function attemptRepair(armor, kit)
 		local condition = GetItemCondition(BAG_WORN, armor)
 		if condition < AR.savedVariables.repairPercentage then
 			RepairItemWithRepairKit(BAG_WORN, armor, BAG_BACKPACK, kit)
+			PlaySound(SOUNDS.INVENTORY_ITEM_REPAIR)
 			if AR.savedVariables.debugMessages then d("Item repaired: "..GetItemName(BAG_WORN, armor)) end
 		end
 	end
@@ -36,10 +40,42 @@ function AR.ChangePlayerCombatState(event, inCombat)
 	
 	if AR.savedVariables.autoRecharge then
 		local soulGemIndex = -1
-		for k, v in pairs(backpack) do
-			if IsItemSoulGem(SOUL_GEM_TYPE_FILLED, BAG_BACKPACK, v.slotIndex) then
-				soulGemIndex = v.slotIndex
-				break
+		
+		if AR.savedVariables.preferCrownGem then
+			--Crown gems
+			for k, v in pairs(backpack) do
+				if IsItemSoulGem(SOUL_GEM_TYPE_FILLED, BAG_BACKPACK, v.slotIndex) and IsItemFromCrownStore(BAG_BACKPACK, v.slotIndex) then
+					soulGemIndex = v.slotIndex
+					break
+				end
+			end
+			
+			--regular gems
+			if soulGemIndex == -1 then
+				for k, v in pairs(backpack) do
+					if IsItemSoulGem(SOUL_GEM_TYPE_FILLED, BAG_BACKPACK, v.slotIndex) and IsItemFromCrownStore(BAG_BACKPACK, v.slotIndex) == false then
+						soulGemIndex = v.slotIndex
+						break
+					end
+				end
+			end
+		else
+			--regular gems
+			for k, v in pairs(backpack) do
+				if IsItemSoulGem(SOUL_GEM_TYPE_FILLED, BAG_BACKPACK, v.slotIndex) and IsItemFromCrownStore(BAG_BACKPACK, v.slotIndex) == false then
+					soulGemIndex = v.slotIndex
+					break
+				end
+			end
+			
+			--crown gems
+			if soulGemIndex == -1 then
+				for k, v in pairs(backpack) do
+					if IsItemSoulGem(SOUL_GEM_TYPE_FILLED, BAG_BACKPACK, v.slotIndex) and IsItemFromCrownStore(BAG_BACKPACK, v.slotIndex) then
+						soulGemIndex = v.slotIndex
+						break
+					end
+				end
 			end
 		end
 		
@@ -55,28 +91,62 @@ function AR.ChangePlayerCombatState(event, inCombat)
 	
 	if AR.savedVariables.autoRepair then
 		local repairKitIndex = -1
-		for k, v in pairs(backpack) do
-			if  IsItemRepairKit(BAG_BACKPACK, v.slotIndex) and
-				IsItemNonCrownRepairKit(BAG_BACKPACK, v.slotIndex) and 
-				IsItemNonGroupRepairKit(BAG_BACKPACK, v.slotIndex) then
-				
-				repairKitIndex = v.slotIndex
-				break
-			end
-		end
 		
-		if repairKitIndex == -1 then
-			if AR.savedVariables.debugMessages then d("Player does not have any repair kits to repair armor!") end
+		if AR.savedVariables.useCrownRepair == false then
+			for k, v in pairs(backpack) do
+				if  IsItemRepairKit(BAG_BACKPACK, v.slotIndex) and
+					IsItemNonCrownRepairKit(BAG_BACKPACK, v.slotIndex) and 
+					IsItemNonGroupRepairKit(BAG_BACKPACK, v.slotIndex) then
+					
+					repairKitIndex = v.slotIndex
+					break
+				end
+			end
+		
+			if repairKitIndex == -1 then
+				if AR.savedVariables.debugMessages then d("Player does not have any repair kits to repair armor!") end
+			else
+				attemptRepair(EQUIP_SLOT_CHEST, repairKitIndex)
+				attemptRepair(EQUIP_SLOT_FEET, repairKitIndex)
+				attemptRepair(EQUIP_SLOT_HAND, repairKitIndex)
+				attemptRepair(EQUIP_SLOT_HEAD, repairKitIndex)
+				attemptRepair(EQUIP_SLOT_LEGS, repairKitIndex)
+				attemptRepair(EQUIP_SLOT_SHOULDERS, repairKitIndex)
+				attemptRepair(EQUIP_SLOT_WAIST, repairKitIndex)
+				attemptRepair(EQUIP_SLOT_BACKUP_OFF, repairKitIndex)
+				attemptRepair(EQUIP_SLOT_OFF_HAND, repairKitIndex)
+			end
 		else
-			attemptRepair(EQUIP_SLOT_CHEST, repairKitIndex)
-			attemptRepair(EQUIP_SLOT_FEET, repairKitIndex)
-			attemptRepair(EQUIP_SLOT_HAND, repairKitIndex)
-			attemptRepair(EQUIP_SLOT_HEAD, repairKitIndex)
-			attemptRepair(EQUIP_SLOT_LEGS, repairKitIndex)
-			attemptRepair(EQUIP_SLOT_SHOULDERS, repairKitIndex)
-			attemptRepair(EQUIP_SLOT_WAIST, repairKitIndex)
-			attemptRepair(EQUIP_SLOT_BACKUP_OFF, repairKitIndex)
-			attemptRepair(EQUIP_SLOT_OFF_HAND, repairKitIndex)
+			for k, v in pairs(backpack) do
+				if GetItemName(BAG_BACKPACK, v.slotIndex) == "Crown Repair Kit" or
+					GetItemName(BAG_BACKPACK, v.slotIndex) == "Bound Crown Repair Kit"then
+					
+					repairKitIndex = v.slotIndex
+					break
+				end
+			end
+			
+			if repairKitIndex == -1 then
+				if AR.savedVariables.debugMessages then d("Player does not have any crown repair kits to repair armor!") end
+			else
+				if GetItemCondition(BAG_WORN, EQUIP_SLOT_CHEST) < AR.savedVariables.repairPercentage or
+					GetItemCondition(BAG_WORN, EQUIP_SLOT_FEET) < AR.savedVariables.repairPercentage or
+					GetItemCondition(BAG_WORN, EQUIP_SLOT_HAND) < AR.savedVariables.repairPercentage or
+					GetItemCondition(BAG_WORN, EQUIP_SLOT_HEAD) < AR.savedVariables.repairPercentage or
+					GetItemCondition(BAG_WORN, EQUIP_SLOT_LEGS) < AR.savedVariables.repairPercentage or
+					GetItemCondition(BAG_WORN, EQUIP_SLOT_SHOULDERS) < AR.savedVariables.repairPercentage or
+					GetItemCondition(BAG_WORN, EQUIP_SLOT_WAIST) < AR.savedVariables.repairPercentage or
+					(DoesItemHaveDurability(BAG_WORN, EQUIP_SLOT_BACKUP_OFF) and GetItemCondition(BAG_WORN, EQUIP_SLOT_BACKUP_OFF) < AR.savedVariables.repairPercentage) or
+					(DoesItemHaveDurability(BAG_WORN, EQUIP_SLOT_OFF_HAND) and GetItemCondition(BAG_WORN, EQUIP_SLOT_OFF_HAND) < AR.savedVariables.repairPercentage) then
+						
+						if inCombat == false then
+							CallSecureProtected("UseItem", BAG_BACKPACK, repairKitIndex)
+							
+							PlaySound(SOUNDS.INVENTORY_ITEM_REPAIR)
+							if AR.savedVariables.debugMessages then d("Armor repaired with crown repair kit!") end
+						end
+				end
+			end
 		end
 	end
 	
@@ -87,18 +157,37 @@ function AR.merchantRepair(eventCode)
 		local backpack = SHARED_INVENTORY:GenerateFullSlotData(nil, BAG_BACKPACK) 
 		local equips = SHARED_INVENTORY:GenerateFullSlotData(nil, BAG_WORN)
 		
-		--TODO: Debug message that tells the player how much they spent.
+		local heldMoney = GetCurrencyAmount(CURT_MONEY, CURRENCY_LOCATION_CHARACTER)
+		local cost = 0
+		local itemsToRepair_Backpack = {}
+		local itemsToRepair_Equips = {}
 		
 		for k, v in pairs(backpack) do
 			if DoesItemHaveDurability(BAG_BACKPACK, v.slotIndex) and GetItemRepairCost(BAG_BACKPACK, v.slotIndex) > 0 then
-				RepairItem(BAG_BACKPACK, v.slotIndex)
+				cost = cost + GetItemRepairCost(BAG_BACKPACK, v.slotIndex)
+				itemsToRepair_Backpack[#itemsToRepair_Backpack+1] = v.slotIndex
 			end
 		end
 		
 		for k, v in pairs(equips) do
 			if DoesItemHaveDurability(BAG_WORN, v.slotIndex) and GetItemRepairCost(BAG_WORN, v.slotIndex) > 0 then
-				RepairItem(BAG_WORN, v.slotIndex)
+				cost = cost + GetItemRepairCost(BAG_WORN, v.slotIndex)
+				itemsToRepair_Equips[#itemsToRepair_Equips+1] = v.slotIndex
 			end
+		end
+		
+		if cost < heldMoney then
+			for k, v in pairs(itemsToRepair_Backpack) do
+				RepairItem(BAG_BACKPACK, v)
+			end
+			
+			for k, v in pairs(itemsToRepair_Equips) do
+				RepairItem(BAG_WORN, v)
+			end
+			
+			if AR.savedVariables.debugMessages then d("You spent "..cost.." gold to repair your gear.") end
+		else
+			if AR.savedVariables.debugMessages then d("You couldn't afford "..cost.." gold to repair your gear.") end
 		end
 	end
 end
@@ -121,11 +210,16 @@ function AR.Initialize()
         buttonText = "RESET",
         clickHandler = function(control, button)
 			AR.savedVariables.debugMessages = AR.defaults.debugMessages
+			
 			AR.savedVariables.autoRecharge = AR.defaults.autoRecharge
 			AR.savedVariables.autoRepair = AR.defaults.autoRepair
 			AR.savedVariables.autoMerchant = AR.defaults.autoMerchant
+			
 			AR.savedVariables.rechargePercentage = AR.defaults.rechargePercentage
 			AR.savedVariables.repairPercentage = AR.defaults.repairPercentage
+			
+			AR.savedVariables.preferCrownGem = AR.defaults.preferCrownGem
+			AR.savedVariables.useCrownRepair = AR.defaults.useCrownRepair
 		end,
         disable = function() return areSettingsDisabled end,
     }
@@ -178,6 +272,20 @@ function AR.Initialize()
         disable = function() return areSettingsDisabled end,
     }
 	
+	local useCrownGem = {
+        type = LibHarvensAddonSettings.ST_CHECKBOX, --setting type
+        label = "Prefer Crown Gems", 
+        tooltip = "Change whether the addon will search for crown soul gems or regular soul gems first.",
+        default = AR.savedVariables.preferCrownGem,
+        setFunction = function(state) 
+            AR.savedVariables.preferCrownGem = state
+        end,
+        getFunction = function() 
+            return AR.savedVariables.preferCrownGem
+        end,
+        disable = function() return areSettingsDisabled end,
+    }
+	
 	local toggle_merchant = {
         type = LibHarvensAddonSettings.ST_CHECKBOX, --setting type
         label = "Toggle Auto Merchant Repair", 
@@ -226,10 +334,25 @@ function AR.Initialize()
         disable = function() return areSettingsDisabled end,
     }
 	
+	local useCrownRepair= {
+        type = LibHarvensAddonSettings.ST_CHECKBOX, --setting type
+        label = "Use Crown Repair Kits", 
+        tooltip = "Toggle whether this addon will try to repair with regular repair kits or crown repair kits.\n\n"..
+		"Note: Crown armor repair kit checks can only occur as you exit combat, not as you enter it.",
+        default = AR.savedVariables.useCrownRepair,
+        setFunction = function(state) 
+            AR.savedVariables.useCrownRepair = state
+        end,
+        getFunction = function() 
+            return AR.savedVariables.useCrownRepair
+        end,
+        disable = function() return areSettingsDisabled end,
+    }
+	
 	
 	settings:AddSettings({generalSection, resetDefaults, toggle_debug})
-	settings:AddSettings({rechargeSection, toggle_recharge, slider_recharge})
-	settings:AddSettings({repairSection, toggle_merchant, toggle_repair, slider_repair})
+	settings:AddSettings({rechargeSection, toggle_recharge, slider_recharge, useCrownGem})
+	settings:AddSettings({repairSection, toggle_merchant, toggle_repair, slider_repair, useCrownRepair})
 	
 	
 	EVENT_MANAGER:RegisterForEvent(AR.name, EVENT_PLAYER_COMBAT_STATE, AR.ChangePlayerCombatState)
